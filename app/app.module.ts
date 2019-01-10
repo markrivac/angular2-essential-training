d1df70d009d1a3edbd0b9822d2ea1f897e0e363d
Mark Octavio Rivera Acosta <markhome@Marks-MacBook-Pro.local>
2019-01-09 23:43:06 -0600

    Adding new components

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
+// Import all new components we want to add to the app
+import { MediaItemComponent } from './media-item.component';
+
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    MediaItemComponent
   ],
   bootstrap: [
     AppComponent
