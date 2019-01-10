3e659857b867d094aa43cbcf018c192064499cd3
Mark Octavio Rivera Acosta <markhome@Marks-MacBook-Pro.local>
2019-01-09 23:34:27 -0600

    Adding CSS

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mw-app',
-  templateUrl: 'app/app.component.html'
+  templateUrl: 'app/app.component.html',
+  styleUrls:[
+    'app/app.component.css'
+  ]
+  // styles:[
+  //   `
+  //     h1 { color: #ffffff;}
+  //     .description{
+  //       font-style: italic;
+  //       color: green;
+  //     }
+  //   `
+  // ]
 })
 export class AppComponent {}
